608282e579ada590555af9ddc9ee251a2dcbc168
Paras Sanghavi <paras@disambiguo.us>
2021-06-16 18:31:03 -0700

    wip

--- lib/store.ts
+++ lib/store.ts
@@ -3,13 +3,11 @@ import { Seed, randSeed } from "./seed";
 
 export type State = {
   canvas: HTMLCanvasElement | null;
-  mode: "design" | "viewing";
   set: SetState<State>;
   mySeed: Seed;
 };
 const useStore = create<State>((set) => ({
   set,
-  mode: "design",
   canvas: null,
   mySeed: randSeed(),
 }));
